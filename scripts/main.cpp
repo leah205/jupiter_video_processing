1088e9cf3fd360978345edb38c0b7580f052ed90
Leah Tiktin <leahtiktin@gmail.com>
2026-09-05 07:05:39 +0200

    fix: stacking bug

--- scripts/main.cpp
+++ scripts/main.cpp
@@ -1,25 +1,52 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
 
-// compile with g++ -o main main.cpp
-// g++ main.cpp -o my_program $(pkg-config --cflags --libs opencv4) - turn into make file
+// g++ main.cpp  -g -o my_program $(pkg-config --cflags --libs opencv4) - turn into make file
 
-void get_planet_mask(cv::Mat frame, cv::Mat mask)
+void get_array_info(cv::Mat frame)
 {
+    // 320
+    std::cout << "rows: " << frame.rows << std::endl;
+    // 320
+    std::cout << "cols: " << frame.cols << std::endl;
+    std::cout << "channels: " << frame.channels() << std::endl;
+    // CV_8U (char)
+    std::cout << "data type: " << frame.type() << std::endl;
+    std::cout << "depth: " << frame.depth() << std::endl;
+}
+
+/**
+ * @brief Get the planet mask object
+ *
+ * Creates planet mask based on hard coded intensity threshold
+ *
+ * @param frame
+ * @param mask
+ */
+
+void get_planet_mask(cv::Mat frame, cv::Mat &mask)
+{
+
     cv::Mat blurred;
-    // frame.copyTo(blurred);
     // should i blur here?
     cv::GaussianBlur(frame, blurred, cv::Size(3, 3), 0);
     cv::threshold(blurred, mask, 10, 255, cv::THRESH_BINARY);
 };
 
-void compress(cv::Mat frame)
+/**
+ * @brief
+ *
+ * @param frame
+ */
+void preprocess(cv::Mat &frame)
 {
-    cv::extractChannel(frame, frame, 0);
+    cv::normalize(frame, frame, 0, 255, cv::NORM_MINMAX);
 }
 
-void align_frame(cv::Mat frame, cv::Point cm, cv::Point ref)
+void align_frame(cv::Mat &frame, cv::Point cm, cv::Point ref)
 {
+    cv::extractChannel(frame, frame, 0);
+
     double offset_x, offset_y;
     offset_x = cm.x - ref.x;
     offset_y = cm.y - ref.y;
@@ -27,11 +54,8 @@ void align_frame(cv::Mat frame, cv::Point cm, cv::Point ref)
     cv::Mat translation_matrix = (cv::Mat_<double>(2, 3) << 1, 0, -1 * offset_x, 0, 1, -1 * offset_y);
     int height = frame.cols;
     int width = frame.rows;
-    std::cout << "cm x: " << cm.x << " ref x: " << ref.x << std::endl;
 
     cv::warpAffine(frame, frame, translation_matrix, cv::Size(width, height));
-    cv::imshow("aligned", frame);
-    cv::waitKey(0);
 }
 
 cv::Point get_center_of_mass(cv::Mat frame)
@@ -41,8 +65,6 @@ cv::Point get_center_of_mass(cv::Mat frame)
     get_planet_mask(frame, mask);
 
     cv::extractChannel(mask, mask, 0);
-
-    std::cout << mask.channels() << std::endl;
     cv::Moments m = cv::moments(mask, true);
 
     if (std::abs(m.m00) < 1e-8)
@@ -52,50 +74,49 @@ cv::Point get_center_of_mass(cv::Mat frame)
 
     cv::Point p(m.m10 / m.m00, m.m01 / m.m00);
     return p;
-
-    // cv::circle(frame, p, 5, cv::Scalar(128, 0, 0), -1);
-    // cv::imshow("image with center", frame);
-    // cv::waitKey(0);
-    // std::cout
-    //     << "x center " << m.m10 << std::endl;
-    // std::cout << "y center " << m.m01 << std::endl;
 }
 
-// void stack_images(cv::Mat frames[], cv::Mat stacked[], int num_frames)
-// {
-//     int num_rows = frames[0].rows;
-//     int num_cols = frames[0].cols;
-//     // what data type to pout here?
-//     cv::Mat sum_mat = cv::Mat::zeros(num_rows, num_cols, CV_16F);
-//     uchar *sum_ptr = sum_mat.data;
-//     for (int i = 0; i < num_frames; i++)
-//     {
-//         cv::Mat cur = frames[i];
-//         for (int row = 0; row < num_rows; ++row)
-//         {
-//             // why not just use one pointer here?
-//             uchar *p = cur.ptr(row);
-//             for (int col = 0; col < num_cols; ++col)
-//             {
-//                 *sum_ptr += *p;
-//                 p++;
-//             }
-//         }
-//     }
-
-//     cv::Mat stacked = sum_mat / num_frames;
-// }
-
-void get_array_info(cv::Mat frame)
+void stack_images(cv::Mat frames[], cv::Mat stacked, int num_frames)
 {
-    // 320
-    std::cout << "rows: " << frame.rows << std::endl;
-    // 320
-    std::cout << "cols: " << frame.cols << std::endl;
-    std::cout << "channels: " << frame.channels() << std::endl;
-    // CV_8U
-    std::cout << "data type: " << frame.type() << std::endl;
-    std::cout << "depth: " << frame.depth() << std::endl;
+    int num_rows = frames[0].rows;
+    int num_cols = frames[0].cols;
+    cv::Mat sum_mat = cv::Mat::zeros(num_rows, num_cols, CV_32SC1);
+    int *sum_ptr = sum_mat.ptr<int>(0);
+    for (int i = 0; i < num_frames - 1; i++)
+    {
+        if (i == 0)
+        {
+            get_array_info(frames[0]);
+            cv::imshow("frame", frames[i]);
+            cv::waitKey(0);
+        }
+        cv::Mat cur = frames[i];
+        for (int row = 0; row < num_rows; row++)
+        {
+            // why not just use one pointer here?
+            uchar *p = cur.ptr(row);
+            int *sum_ptr = sum_mat.ptr<int>(row);
+            for (int col = 0; col < num_cols; col++)
+            {
+                *sum_ptr += *p;
+                sum_ptr++;
+                p++;
+            }
+        }
+
+        if (i == 0)
+        {
+            cv::Mat display;
+            cv::normalize(sum_mat, display, 0, 255, cv::NORM_MINMAX, CV_8UC1);
+            cv::imshow("sum", display);
+            cv::waitKey(0);
+            break;
+        }
+    }
+    cv::normalize(sum_mat, sum_mat, 0, 1 << 16, cv::NORM_MINMAX);
+    sum_mat.convertTo(sum_mat, CV_16UC1);
+    cv::imshow("sum matrix", sum_mat);
+    cv::waitKey(0);
 }
 
 int main()
@@ -113,11 +134,13 @@ int main()
         std::cout << "Video opened successfully" << std::endl;
     }
 
-    // cv::Mat frames[frame_num];
-    // uchar *cur_frame = frames.ptr(0);
+    cv::Mat translated_frames[frame_num];
+    cv::Mat *cur_frame = translated_frames;
+
     cv::Mat frame;
     bool ret = cap.read(frame);
-    compress(frame);
+    preprocess(frame);
+    get_array_info(frame);
 
     cv::Point ref_cm = get_center_of_mass(frame);
 
@@ -131,11 +154,15 @@ int main()
             break;
         }
 
+        preprocess(frame);
         cv::Point cm = get_center_of_mass(frame);
         align_frame(frame, cm, ref_cm);
-        // frames[]
-    }
 
+        *cur_frame = frame;
+        cur_frame++;
+    }
+    cv::Mat stacked;
+    stack_images(translated_frames, stacked, frame_num);
     cap.release();
     return 0;
 }
